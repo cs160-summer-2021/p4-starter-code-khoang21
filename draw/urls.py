4f1d2d8d94634516ad6079e3747402aad93025d8
Kathy Hoang <khoang21@berkeley.edu>
2021-07-25 23:53:21 -0700

    project 4 done yay

--- draw/urls.py
+++ draw/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path('<str:room_name>/', views.room, name='room'),
     path('bigscreen', views.bigscreen, name='bigscreen'),
     path('form', views.form, name='form'),
+    path('manager', views.manager, name='manager'),
+    path('busser', views.busser, name='busser'),
+    path('chef', views.chef, name='chef'),
+    path('cashier', views.cashier, name='cashier'),
+    path('host', views.host, name='host'),
+    path('server', views.server, name='server'),
 
 ]
--- draw/views.py
+++ draw/views.py
@@ -14,3 +14,21 @@ def bigscreen(request):
 
 def form(request):
     return render(request, 'draw/form.html')
+
+def manager(request):
+    return render(request, 'draw/manager.html')
+
+def busser(request):
+    return render(request, 'draw/busser.html')
+
+def chef(request):
+    return render(request, 'draw/chef.html')
+
+def cashier(request):
+    return render(request, 'draw/cashier.html')
+
+def host(request):
+    return render(request, 'draw/host.html')
+
+def server(request):
+    return render(request, 'draw/server.html')
